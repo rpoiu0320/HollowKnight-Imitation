8f7987672a845763ec59f21fcab3c886ecb793d2
Hyeonuk Ryu <rpoiu0320@gmail.com>
2023-06-27 21:00:42 +0900

    feat : Gamemanager
    GameManager 완료

--- Assets/Managers/Gamemanager.cs
+++ Assets/Managers/Gamemanager.cs
@@ -10,6 +10,13 @@ public class Gamemanager : MonoBehaviour
     private static UIManager uiManager;
     private static DataManager dataManager;
 
+    public static Gamemanager Instance { get { return instance; } }
+    public static PoolManager Pool {  get { return poolManager; } }
+    public static ResourceManager Resource { get { return resourceManager; } }
+    public static UIManager UI { get { return uiManager; } }
+    public static DataManager Data {  get { return dataManager; } }
+
+
     private void Awake()
     {
         if (instance != null)
@@ -25,15 +32,15 @@ public class Gamemanager : MonoBehaviour
 
     private void InitManagers()
     {
-        GameObject resouceObj = new GameObject();
-        resouceObj.name = "ResourceManager";
-        resouceObj.transform.parent = transform;
-        resourceManager = resouceObj.AddComponent<ResourceManager>();
-
         GameObject poolObj = new GameObject();
         poolObj.name = "PollManager";
         poolObj.transform.parent = transform;
         poolManager = poolObj.AddComponent<PoolManager>();
+
+        GameObject resouceObj = new GameObject();
+        resouceObj.name = "ResourceManager";
+        resouceObj.transform.parent = transform;
+        resourceManager = resouceObj.AddComponent<ResourceManager>();
 
         GameObject uiObj = new GameObject();
         uiObj.name = "UIManager";
