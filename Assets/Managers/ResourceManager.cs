8cf8b9a668aa711e9410f52c7c213466438c7e88
Hyeonuk Ryu <rpoiu0320@gmail.com>
2023-06-28 20:07:40 +0900

    feat : ResourceManager
    ResourceManager 가재작

--- Assets/Managers/ResourceManager.cs
+++ Assets/Managers/ResourceManager.cs
@@ -4,5 +4,47 @@ using UnityEngine;
 
 public class ResourceManager : MonoBehaviour
 {
-    
+    Dictionary<string, Object> resources = new Dictionary<string, Object>();
+
+    public T Load<T>(string path) where T : Object
+    {
+        string key = $"{typeof(T)}.{path}";
+
+        if(resources.ContainsKey(key))
+            return resources[key] as T;
+
+        T resource = Resources.Load<T>(path);
+        resources.Add(key, resource);
+        return resource;
+    }
+
+    public T Instantiate<T>(T original, Vector3 position, Transform parent, bool pooling = false) where T : Object
+    {
+        if (pooling)
+            return GameManager.Pool.Get(original, position, parent);
+        else
+            return Object.Instantiate(original, position, Quaternion.identity, parent);
+    }
+
+    public void Destory(GameObject gameObject)
+    {
+        if(GameManager.Pool.IsContain(gameObject))
+            GameManager.Pool.Release(gameObject);
+        else
+            GameManager.Destroy(gameObject);
+    }
+
+    public void Destory(GameObject gameObject, float delay)
+    {
+        if (GameManager.Pool.IsContain(gameObject))
+            StartCoroutine(DelayReleaseRoutine(gameObject, delay));
+        else
+            GameObject.Destroy(gameObject, delay);
+    }
+
+    IEnumerator DelayReleaseRoutine(GameObject gameObject, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        GameManager.Pool.Release(gameObject);
+    }
 }
