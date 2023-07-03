ebe080d6763ac1a9753a16165f9568ad9d6ed195
Hyeonuk Ryu <rpoiu0320@gmail.com>
2023-07-03 16:14:44 +0900

    feat : ShotSoul
    Add ShotSoul End Animation, BumpRoutine

--- Assets/Scripts/Player/Skill/ShotSoul.cs
+++ Assets/Scripts/Player/Skill/ShotSoul.cs
@@ -7,20 +7,26 @@ public class ShotSoul : MonoBehaviour
     [SerializeField] float moveSpeed;
     private PlayerMover playerMover;
     private SpriteRenderer render;
+    private Collider2D collider2d;
+    private Animator shotSoulAnimator;
     private ContactFilter2D contactFilter;
     private float direction;
+    private bool isBump = false;
 
     private void OnEnable()
     {
         playerMover = GameObject.FindWithTag("Player").GetComponent<PlayerMover>();
         render = GetComponent<SpriteRenderer>();
+        collider2d = GetComponent<Collider2D>();
+        shotSoulAnimator = GetComponent<Animator>();
         contactFilter.SetLayerMask(LayerMask.GetMask("Monster"));
         direction = playerMover.LastDirX();
     }
 
     private void Update()
     {
-        Move();
+        if (!isBump)
+            Move();
     }
 
     private void Move()
@@ -36,6 +42,25 @@ public class ShotSoul : MonoBehaviour
             render.flipX = true;
         }
     }
+
+    Coroutine bumpRoutine;
+    IEnumerator BumpRoutine()
+    {
+        Debug.Log("StargBump");
+        isBump = true;
+        shotSoulAnimator.SetTrigger("IsBump");
+        collider2d.enabled = false;
+
+        if (direction > 0)
+            transform.Translate(new Vector3(7f, 0, 0));
+        else if (direction < 0)
+            transform.Translate(new Vector3(7f, 0, 0));
+
+        yield return new WaitForSeconds(0.4f);
+
+        StopCoroutine(bumpRoutine);
+        GameManager.Resource.Destory(gameObject);
+    }
     
     private void OnTriggerEnter2D(Collider2D target)
     {
@@ -45,14 +70,13 @@ public class ShotSoul : MonoBehaviour
         }
         else if(target.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
-            Debug.Log("Ground Enter");
-            GameManager.Resource.Destory(gameObject);
+            bumpRoutine = StartCoroutine(BumpRoutine());
         }
     }
 
     private void OnTriggerExit2D(Collider2D target)
     {
-        if (target.gameObject.layer == LayerMask.NameToLayer("MapBoundary"))
+        if (!isBump && target.gameObject.layer == LayerMask.NameToLayer("MapBoundary"))
         {
             GameManager.Resource.Destory(gameObject);
         }
