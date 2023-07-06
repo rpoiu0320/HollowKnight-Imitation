cba39b690e7ba8f9faeb2ed3a59ab6a52ca052d8
Hyeonuk Ryu <rpoiu0320@gmail.com>
2023-07-07 08:31:28 +0900

    feat : PlayerAttacker CommonAttack
    CommonAttack 시 Monster or Spikes 이면 바라보는 반대방향으로 밀려나도록 가구현

--- Assets/Scripts/Thorn.cs
+++ Assets/Scripts/Thorn.cs
@@ -11,6 +11,28 @@ public class Thorn : MonoBehaviour, IHittable
 
     private void test()
     {/*
+        animator.SetTrigger("Attack");
+        commonAttackAnimator.SetTrigger("Attack");
+        Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(commonAttackPoint.position, commonAttackRange, 0, hitMask);
+
+        foreach (Collider2D collider in collider2Ds)
+        {
+            if (collider.gameObject.layer != LayerMask.NameToLayer("Monster") || collider.gameObject.layer != LayerMask.NameToLayer("Spikes"))
+                continue;
+
+            IHittable hittable = collider.GetComponent<IHittable>();
+            hittable?.TakeHit(player.data.Player[0].attackDamage);
+
+            if (playerMover.LastDirX() > 0)
+                playerMover.rb.Velocity(Vector2.left * 10);
+            else if (playerMover.LastDirX() < 0)
+                playerMover.rb.Velocity(Vector2.right * 10);
+            
+        }*/
+
+
+
+        /*
         Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(jumpAttackDownPoint.position, jumpAttackDownRange, 0, hitMask);
 
         foreach (Collider2D collider in collider2Ds)
