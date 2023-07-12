1456d7a05aeb3e633a8f819bc2342d688489834c
Hyeonuk Ryu <rpoiu0320@gmail.com>
2023-07-12 19:01:07 +0900

    refactor : PlayerHit
    Player에 있는 Hit부분을 PlayerHit으로 이동

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,20 +4,12 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Player : MonoBehaviour, IHittable
+public class Player : MonoBehaviour
 {
     [SerializeField] Animator hitAnimator;
     // TODO : Data 연동 
     public PlayerData data;
     public DataManager dataManager;
-    private PlayerMover playerMover;
-    private SpriteRenderer render;
-    private Animator playerAnimator;
-    private float hitTime;
-    private bool isTwinkling;
-    private int twinklingCount;
-    private int twinklingColor;
-    private int curHp;
     public UnityEvent OnCameraNoise;
 
     private void Awake()
@@ -25,98 +17,5 @@ public class Player : MonoBehaviour, IHittable
         // TODO : Data 연동 
         data = new PlayerData();
         dataManager = new DataManager();
-        playerMover = GetComponent<PlayerMover>();
-        render = GetComponent<SpriteRenderer>();
-        playerAnimator = GetComponent<Animator>();
-    }
-
-    private void Start()
-    {
-        curHp = dataManager.CurHp;
-    }
-
-    private void Update()
-    {
-        Debug.Log(curHp);
-    }
-
-    public void TakeHit(int damage)
-    {
-        hitRoutine = StartCoroutine(HitRotine());
-    }
-
-    Coroutine hitRoutine;
-    IEnumerator HitRotine()
-    {
-        hitTime = 0;
-        OnCameraNoise?.Invoke();
-        hitAnimator.SetTrigger("OnHit");
-        playerAnimator.SetTrigger("Hit");
-        playerMover.LimitMove(true);
-        gameObject.layer = LayerMask.NameToLayer("Default");
-        isTwinkling = true;
-        twinklingRoutine = StartCoroutine(TwinklingRoutine());
-        Time.timeScale = 0;
-
-        yield return new WaitForSecondsRealtime(0.2f);
-
-        Time.timeScale = 1;
-        
-        while (hitTime < 0.2f)
-        {
-            if (playerMover.LastDirX() > 0)
-                transform.Translate(new Vector3(-30 * Time.deltaTime, 0));
-            else if (playerMover.LastDirX() < 0)
-                transform.Translate(new Vector3(30 * Time.deltaTime, 0));
-
-            hitTime += Time.deltaTime;
-
-            yield return null;
-        }
-
-        playerMover.LimitMove(false);
-
-        yield return new WaitForSeconds(0.8f);
-
-        isTwinkling = false;
-        twinklingColor = 255;
-        render.color = new Color(twinklingColor, twinklingColor, twinklingColor);
-        gameObject.layer = LayerMask.NameToLayer("Player");
-
-        yield break;
-    }
-
-    Coroutine twinklingRoutine;
-    IEnumerator TwinklingRoutine()
-    {
-        twinklingCount = 3;
-
-        while(isTwinkling)
-        {
-            switch(twinklingCount % 3)
-            {
-                case 0:
-                    twinklingColor = 0; 
-                    break;
-                case 1:
-                    twinklingColor = 125;
-                    break;
-                case 2:
-                    twinklingColor = 255;
-                    break;
-                default:
-                    break;
-            }
-
-            twinklingCount++;
-            render.color = new Color(twinklingColor, twinklingColor, twinklingColor);
-
-            yield return new WaitForSeconds(0.1f);
-        }
-
-        twinklingColor = 255;
-        render.color = new Color(twinklingColor, twinklingColor, twinklingColor);
-
-        yield break;
     }
 }
--- Assets/Scripts/Player/PlayerHit.cs
+++ Assets/Scripts/Player/PlayerHit.cs
@@ -1,11 +1,119 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using Cinemachine;
 
 public class PlayerHit : MonoBehaviour, IHittable
 {
+    [SerializeField] Animator hitAnimator;
+    // TODO : Data 연동 
+    public DataManager data;
+    private PlayerMover playerMover;
+    private SpriteRenderer render;
+    private Animator playerAnimator;
+    private float hitTime;
+    private bool isTwinkling;
+    private int twinklingCount;
+    private int twinklingColor;
+    private int curHp;
+    public UnityEvent OnCameraNoise;
+
+    private void Awake()
+    {
+        data = new DataManager();
+        playerMover = GetComponent<PlayerMover>();
+        render = GetComponent<SpriteRenderer>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        curHp = data.CurHp;
+    }
+
+    private void Update()
+    {
+        Debug.Log(curHp);
+    }
+
     public void TakeHit(int damage)
     {
-        Debug.Log("플레이어 체력 감소");
+        hitRoutine = StartCoroutine(HitRotine());
+    }
+
+    Coroutine hitRoutine;
+    IEnumerator HitRotine()
+    {
+        hitTime = 0;
+        OnCameraNoise?.Invoke();
+        hitAnimator.SetTrigger("OnHit");
+        playerAnimator.SetTrigger("Hit");
+        playerMover.LimitMove(true);
+        gameObject.layer = LayerMask.NameToLayer("Default");    // 판단을 layer를 기준으로 하기에 피격 시 일시 무적을 위함
+        isTwinkling = true;
+        twinklingRoutine = StartCoroutine(TwinklingRoutine());
+        Time.timeScale = 0;
+
+        yield return new WaitForSecondsRealtime(0.2f);
+
+        Time.timeScale = 1;
+
+        while (hitTime < 0.2f)
+        {
+            if (playerMover.LastDirX() > 0)
+                transform.Translate(new Vector3(-30 * Time.deltaTime, 0));
+            else if (playerMover.LastDirX() < 0)
+                transform.Translate(new Vector3(30 * Time.deltaTime, 0));
+
+            hitTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        playerMover.LimitMove(false);
+
+        yield return new WaitForSeconds(0.8f);
+
+        isTwinkling = false;
+        twinklingColor = 255;
+        render.color = new Color(twinklingColor, twinklingColor, twinklingColor);
+        gameObject.layer = LayerMask.NameToLayer("Player");
+
+        yield break;
+    }
+
+    Coroutine twinklingRoutine;
+    IEnumerator TwinklingRoutine()
+    {
+        twinklingCount = 3;
+
+        while (isTwinkling)
+        {
+            switch (twinklingCount % 3)
+            {
+                case 0:
+                    twinklingColor = 0;
+                    break;
+                case 1:
+                    twinklingColor = 125;
+                    break;
+                case 2:
+                    twinklingColor = 255;
+                    break;
+                default:
+                    break;
+            }
+
+            twinklingCount++;
+            render.color = new Color(twinklingColor, twinklingColor, twinklingColor);
+
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        twinklingColor = 255;
+        render.color = new Color(twinklingColor, twinklingColor, twinklingColor);
+
+        yield break;
     }
 }
