15944949767c4d96516272b2a840fb9e9df7724e
Hyeonuk Ryu <rpoiu0320@gmail.com>
2024-01-16 16:33:01 +0900

    feat : HpUI
    CurHp를 Stack 형식으로 변경

--- Assets/Scripts/UI/Player/HpUI.cs
+++ Assets/Scripts/UI/Player/HpUI.cs
@@ -10,39 +10,51 @@ public class HpUI : MonoBehaviour
     [SerializeField] Sprite onHpImage;
     [SerializeField] Sprite nonHpImage;
 
-    private Image[] hpImages; 
+    private Image[] maxHpImages;
+    private Stack<Image> curHpImages = new Stack<Image>();
 
     private void Start()
     {
-        hpImages = new Image[GameManager.Data.MaxHp];           // Data, UI Manager끼리 충돌 방지를 위해 Awake 대신 Start
+        maxHpImages = new Image[GameManager.Data.MaxHp];           // Data, UI Manager끼리 충돌 방지를 위해 Awake 대신 Start
         SetUpHp();
+    }
+
+    private void Update()
+    {
+        Debug.Log(curHpImages.Count);
     }
 
     private void SetUpHp()
     {
-        for (int i = 0; i < hpImages.Length; i++)
+        for (int i = 0; i < maxHpImages.Length; i++)
         {
             Image hpImage = Instantiate(hpPrefab, transform);           // UI - Hp의 하위로 생성
             hpImage.rectTransform.anchoredPosition                      // 위치 조정
                 = new Vector2(hpImage.rectTransform.anchoredPosition.x + 65 * i, hpImage.rectTransform.anchoredPosition.y);
 
             if (GameManager.Data.CurHp > i)
+            {
                 hpImage.sprite = onHpImage;
+                curHpImages.Push(hpImage);
+            }
             else
                 hpImage.sprite = nonHpImage;
 
             hpImage.gameObject.name = $"HpImage{i}";
-            hpImages[i] = hpImage;
+            maxHpImages[i] = hpImage;
         }
     }
 
     public void IncreaseCurHpUI()
     {
-        hpImages[GameManager.Data.CurHp + 1].sprite = onHpImage;
+        curHpImages.Push(maxHpImages[GameManager.Data.CurHp + 1]);
+        curHpImages.Peek().sprite = onHpImage; 
     }
     
     public void DecreaseCurHpUI()
     {
-        hpImages[GameManager.Data.CurHp].sprite = nonHpImage;
+        curHpImages.Peek().sprite = onHpImage;
+        curHpImages.Pop();
+        maxHpImages[GameManager.Data.CurHp].sprite = nonHpImage;
     }
 }
